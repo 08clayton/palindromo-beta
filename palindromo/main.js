7eca421b673a7b7dbb509fd11c08a4479a343b70
08clayton <claytonfilhosilva@gmail.com>
2025-03-19 16:59:40 -0300

    Add files via upload

--- palindromo/main.js
+++ palindromo/main.js
@@ -8,9 +8,31 @@ function checkPalindromo() {
 
     const cleanedInput = input.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     const reversedInput = cleanedInput.split('').reverse().join('');
+    
+    result.classList.add('visible');
     if (cleanedInput === reversedInput) {
         result.textContent = `${input} is a palindrome.`;
+        result.style.color = '#4CAF50'; 
     } else {
         result.textContent = `${input} is not a palindrome.`;
+        result.style.color = '#f44336';
     }
 }
+
+function clearFields() {
+    document.getElementById('text-input').value = '';
+    document.getElementById('result').classList.remove('visible');
+    document.getElementById('result').textContent = '';
+}
+
+document.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      checkPalindromo();
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Delete') {
+      clearFields();
+    }
+});
