819933cf76e53544197641eb1a11a5ef0ece219f
08clayton <claytonfilhosilva@gmail.com>
2025-03-20 10:51:52 -0300

    Add files via upload

--- palindromo/main.js
+++ palindromo/main.js
@@ -1,38 +1,49 @@
 function checkPalindromo() {
-    const input = document.getElementById('text-input').value;
-    const result = document.getElementById('result');
-    if (!input) {
-        alert('Please input a value');
-        return;
-    }
+  const input = document.getElementById('text-input').value;
+  const result = document.getElementById('result');
+  const clearBtn = document.getElementById('clear-btn');
+  const historyList = document.getElementById('history-list');
 
-    const cleanedInput = input.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    const reversedInput = cleanedInput.split('').reverse().join('');
-    
-    result.classList.add('visible');
-    if (cleanedInput === reversedInput) {
-        result.textContent = `${input} is a palindrome.`;
-        result.style.color = '#4CAF50'; 
-    } else {
-        result.textContent = `${input} is not a palindrome.`;
-        result.style.color = '#f44336';
-    }
+  if (!input) {
+    alert('Please input a value');
+    return;
+  }
+  clearBtn.style.display = 'inline-block';
+
+  const cleanedInput = input.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  const reversedInput = cleanedInput.split('').reverse().join('');
+
+  result.classList.add('visible');
+
+  if (cleanedInput === reversedInput) {
+    result.textContent = `${input} is a palindrome.`;
+    result.style.color = '#4CAF50';
+    historyList.innerHTML += `<li class="palindrome">${input} is a palindrome.</li>`;
+  } else {
+    result.textContent = `${input} is not a palindrome.`;
+    result.style.color = '#f44336';
+    historyList.innerHTML += `<li class="not-palindrome">${input} is not a palindrome.</li>`;
+  }
+  if (historyList.children.length > 1000) {
+    historyList.removeChild(historyList.lastElementChild);
+}
 }
 
 function clearFields() {
-    document.getElementById('text-input').value = '';
-    document.getElementById('result').classList.remove('visible');
-    document.getElementById('result').textContent = '';
+  document.getElementById('text-input').value = '';
+  document.getElementById('result').classList.remove('visible');
+  document.getElementById('result').textContent = '';
+  document.getElementById('clear-btn').style.display = 'none';
 }
 
-document.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-      checkPalindromo();
-    }
-  });
+document.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    checkPalindromo();
+  }
+});
 
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'Delete') {
-      clearFields();
-    }
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Delete') {
+    clearFields();
+  }
 });
